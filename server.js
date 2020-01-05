679cf4275163ce4ead9ac3cbc0dc3792ba8f1ede
alan <alanarostegui@hotmail.com>
2020-01-04 23:36:19 -0600

    trabajando con el body

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
 app.use(router);
 
 router.get('/', (req,res) => {
@@ -9,6 +12,7 @@ router.get('/', (req,res) => {
 });
 
 router.post('/', (req,res) => {
+    console.log(req.body);
     res.send('Mensaje añadido (Hola desde post)');
 });
 
