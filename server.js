a75eaf9612f02c5a88b5bc2bfc2cf2f70966a149
alan <alanarostegui@hotmail.com>
2020-01-15 01:31:37 -0600

    respuestas coherentes

--- server.js
+++ server.js
@@ -1,36 +1,43 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+
+const response = require("./network/response");
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 app.use(router);
 
-router.get('/', (req,res) => {
-    console.log(req.headers);
-    res.header({
-        "custom-header": "Nuestro valor personalizado",
-    })
-    res.send('Lista de mensajes (hola desde get)');
+router.get("/", (req, res) => {
+  console.log(req.headers);
+  res.header({
+    "custom-header": "Nuestro valor personalizado"
+  });
+  response.success(req, res, "Lista de mensajes");
 });
 
-router.post('/', (req,res) => {
-    res.status(201).send([{ error: '', body: 'mensaje eliminado'}]);
+router.post("/message", (req, res) => {
+  console.log(req.query);
+  if (req.query.error == "ok") {
+    response.error(req, res, "Error simulado", 400);
+  } else {
+    response.success(req, res, "Creado correctamente", 201);
+  }
 });
 
-router.put('/', (req,res) => {
-    res.send('Mensaje editado (Hola desde put)');
+router.put("/", (req, res) => {
+  res.send("Mensaje editado (Hola desde put)");
 });
 
-router.patch('/', (req,res) => {
-    res.send('Mensaje editado (Hola desde patch)');
+router.patch("/", (req, res) => {
+  res.send("Mensaje editado (Hola desde patch)");
 });
 
-router.delete('/', (req,res) => {
-    res.send('Mensaje eliminado (Hola desde delete');
+router.delete("/", (req, res) => {
+  res.send("Mensaje eliminado (Hola desde delete");
 });
 
 app.listen(3000, () => {
-    console.log('Telegram listening on port 3000')
+  console.log("Telegram listening on port 3000");
 });
