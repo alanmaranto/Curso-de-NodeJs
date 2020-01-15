97fcb0bcc6314efc1a7621a602fb7b667568d4d8
alan <alanarostegui@hotmail.com>
2020-01-15 01:46:20 -0600

    errores: como presentarlos e implicaciones en la seguridad

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ router.get("/", (req, res) => {
 router.post("/message", (req, res) => {
   console.log(req.query);
   if (req.query.error == "ok") {
-    response.error(req, res, "Error simulado", 400);
+    response.error(req, res, "Error inesperado", 500, 'Es solo una simulacion de los errores');
   } else {
     response.success(req, res, "Creado correctamente", 201);
   }
