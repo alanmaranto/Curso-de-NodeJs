f0648706aac616fd707c0f1fc6497c6148d8b8dc
alan <alanarostegui@hotmail.com>
2020-01-15 23:27:35 -0600

    MongoDB 1: Almacenar y leer datos

--- components/message/store.js
+++ components/message/store.js
@@ -1,14 +1,22 @@
-const list = [];
+const db = require("mongoose");
+const Model = require("./model");
 
-const addMessage = (message) => {
-    list.push(message);
+const uri = "mongodb+srv://db_user_telegram:rgI8WhoyqiBNNWj2@cluster0-7l7x7.mongodb.net/telegram"
+db.Promise = global.Promise;
+db.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('[db] Base De Datos Conectada con éxito'))
+    .catch(err => console.error('[db]', err));
+
+const addMessage = message => {
+    const myMessage = new Model(message);
+    myMessage.save();
 };
 
 const getMessages = () => {
-    return list;
+  return list;
 };
 
 module.exports = {
-    add: addMessage,
-    list: getMessages,
-}
+  add: addMessage,
+  list: getMessages
+};
