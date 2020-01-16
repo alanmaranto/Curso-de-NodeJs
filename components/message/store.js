73ea1d9dde7f27e6340d379a1bfbc3041de0a62c
alan <alanarostegui@hotmail.com>
2020-01-16 01:10:07 -0600

    Gestionar conexiones a la base de datos desde la api

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,7 @@
-const db = require("mongoose");
+
 const Model = require("./model");
 
-const uri = "mongodb+srv://db_user_telegram:rgI8WhoyqiBNNWj2@cluster0-7l7x7.mongodb.net/telegram"
-db.Promise = global.Promise;
-db.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('[db] Base De Datos Conectada con éxito'))
-    .catch(err => console.error('[db]', err));
+
 
 const addMessage = message => {
     const myMessage = new Model(message);
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 
 const router = require('./network/routes');
+const db = require('./db');
+
+db("mongodb+srv://db_user_telegram:rgI8WhoyqiBNNWj2@cluster0-7l7x7.mongodb.net/telegram")
 
 const app = express();
 
